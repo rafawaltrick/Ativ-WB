f780798fc0f01605452fc481b0379676d85d1613
Rodrigo Ribeiro <rodrigo.rsantos40@gmail.com>
2022-06-21 23:03:56 -0300

    Inserido conexão com banco de dados.

---
+++ Backend/models/cliente.js
@@ -0,0 +1,48 @@
+import { Sequelize } from "sequelize";
+import db from "../config/db.js"
+
+const cliente = db.define('cliente',{
+    id:{
+        type:Sequelize.STRING,
+        allowNull:true,
+        primaryKey: true
+    },
+    nome:{
+        type:Sequelize.STRING,
+        allowNull:true,
+    },
+    nomeSocial:{
+        type:Sequelize.STRING,
+        allowNull:true,
+    },
+    cpf:{
+        type:Sequelize.INTEGER,
+        allowNull:true,
+    },
+    rg:{
+        type:Sequelize.INTEGER,
+        allowNull:true,
+    },
+    dataCadastro:{
+        type:Sequelize.DATE,
+        allowNull:true,
+    },
+    telefones:{
+        type:Sequelize.INTEGER,
+        allowNull:true,
+    },
+    produtosConsumidos:{
+        type:Sequelize.STRING,
+        allowNull:true,
+    },
+    servicosConsumidos:{
+        type:Sequelize.STRING,
+        allowNull:true,
+    },
+    genero:{
+        type:Sequelize.STRING,
+        allowNull:true,
+    }
+})
+
+export default cliente
